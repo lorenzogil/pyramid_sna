f96e6fd03b5e2076ff8b2f79ec3081173e858c34
ant30 <ant30tx@gmail.com>
2013-12-26 11:26:49 +0100

    Implemented the Microsoft Live Connect oauth2 backend

--- pyramid_sna/__init__.py
+++ pyramid_sna/__init__.py
@@ -96,7 +96,46 @@ def include_facebook(config):
         return False
 
 
+def include_liveconnect(config):
+    settings = config.registry.settings
+
+    for key, default in (
+        ('client_id', None),
+        ('client_secret', None),
+        ('callback', None),
+        ('auth_uri', 'https://login.live.com/oauth20_authorize.srf'),
+        ('token_uri', 'https://login.live.com/oauth20_token.srf'),
+        ('basic_information_url', 'https://apis.live.net/v5.0/me'),
+        ('scope', 'wl.basic wl.emails'),
+    ):
+        option = 'liveconnect_%s' % key
+        settings[option] = read_setting_from_env(settings, option, default)
+
+    if (
+        settings['liveconnect_client_id']
+        and settings['liveconnect_client_secret']
+        and settings['liveconnect_callback']
+    ):
+        config.add_route('liveconnect_login', '/liveconnect/login')
+        config.add_view('pyramid_sna.liveconnect.liveconnect_login',
+                        route_name='liveconnect_login',
+                        renderer='string')
+
+        config.add_route('liveconnect_callback', '/liveconnect/callback')
+        config.add_view('pyramid_sna.liveconnect.liveconnect_callback',
+                        route_name='liveconnect_callback',
+                        renderer='string')
+
+        callback = config.maybe_dotted(settings['liveconnect_callback'])
+        settings['liveconnect_callback'] = callback
+
+        return True
+    else:
+        return False
+
+
 def includeme(config):
     settings = config.registry.settings
     settings['google_auth_enabled'] = include_google(config)
     settings['facebook_auth_enabled'] = include_facebook(config)
+    settings['liveconnect_auth_enabled'] = include_liveconnect(config)
